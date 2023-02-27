b2c99bf0b8bedcd60dd5931f11e5e1e9687937c3
koro <me@krbk.dev>
2023-02-27 18:47:04 +0100

    :sparkles: Fix some errors and rewrite for proper code usage

--- lib/tests/gqlQuery.test.ts
+++ lib/tests/gqlQuery.test.ts
@@ -12,17 +12,17 @@ describe("gqlQuery", () => {
 
     it("should return a valid GraphQL query", () => {
         const result = gqlQuery("query", "test", ["test"]);
-        assert.equal(result, "query { test { test  } }");
+        assert.equal(result, "query { test { test } }");
     });
 
     it("should return a valid query with multiple fields", () => {
         const result = gqlQuery("query", "test", ["test", "test2"]);
-        assert.equal(result, "query { test { test test2  } }");
+        assert.equal(result, "query { test { test test2 } }");
     });
 
     it("should be able to use objects", () => {
         const result = gqlQuery("query", "test", [{test: ["test", "test2"]}, "foo", {bar: ["hello"]}]);
-        assert.equal(result, "query { test { test { test test2 } foo bar { hello }  } }");
+        assert.equal(result, "query { test { test { test test2 } foo bar { hello } } }");
     });
 
     it("should make a correct graphql request", async () => {
@@ -67,7 +67,7 @@ describe("gqlQuery", () => {
 
     it("should be able to use more objects", () => {
         const result = gqlQuery("query", "test", [{test: ["test", "test2", {test3: ["test4"]}]}]);
-        assert.equal(result, "query { test { test { test test2 test3 { test4 } }  } }");
+        assert.equal(result, "query { test { test { test test2 test3 { test4 } } } }");
     });
 
 })
--- lib/utils/gqlQuery.ts
+++ lib/utils/gqlQuery.ts
@@ -2,7 +2,7 @@
  * Util to make a GraphQL query
  */
 
-export default function gqlQuery(type: string, queryName: string, fields: Array<string | object>) : string {
+export default function gqlQuery(type: string, queryName: string, fields: Array<string | object>, isAdded?: boolean) : string {
     let fieldString = "";
     for (const field of fields) {
         if (typeof field === "string") {
@@ -11,9 +11,15 @@ export default function gqlQuery(type: string, queryName: string, fields: Array<
             const key = Object.keys(field)[0];
             // @ts-ignore
             const value = field[key];
-            fieldString += key + " { " + value.join(" ") + " } ";
+            // We could do fieldString += `${key} { ${value.join(" ")} }` but that would not work if we use objects in the array
+            // so we have to do it recursively.
+            fieldString += `${key} { ${gqlQuery("", "", value, true)} } `;
         }
     }
 
-    return `${type} { ${queryName} { ${fieldString} } }`;
+    if (isAdded) {
+        return fieldString;
+    }
+
+    return `${type} { ${queryName} { ${fieldString} } }`.replace(/ {2}/g," ");
 }
